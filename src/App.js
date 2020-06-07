b4a2186a475d3362893d85f35435ec031b95a513
joker7t <batoan131196@gmail.com>
2020-06-07 19:39:27 +0700

    config firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import Home from './components/home/Home';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
-
+import 'firebase';
 
 function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAo_89q5H-ff1Vt5XSeYQqE5cUzsHuIeUw",
+    authDomain: "mcfc-6b482.firebaseapp.com",
+    databaseURL: "https://mcfc-6b482.firebaseio.com",
+    projectId: "mcfc-6b482",
+    storageBucket: "mcfc-6b482.appspot.com",
+    messagingSenderId: "1017664093720",
+    appId: "1:1017664093720:web:a9d2a9ad33159450740d6d",
+    measurementId: "G-53P1W9XJEH"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+
+export {
+    firebaseDB,
+    firebaseMatches
+};
